d4b87a35bd3baf7b8ec0ec4e3129d29c86baaa08
bilal-ahmed07 <bilalahmed.15952@gmail.com>
2025-03-18 12:09:14 +0500

    main

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,11 @@
 import 'package:blog_app_flutter/pages/home_page.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
+
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  Firebase.initializeApp();
   runApp(const MyApp());
 }
 
